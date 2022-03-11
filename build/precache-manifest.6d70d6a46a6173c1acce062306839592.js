354d8107dd740e79b7b3c78b26736f8e06fa9a21
Can Surer <cansurer@outlook.com>
2022-03-11 19:10:48 +0300

    pages

---
+++ build/precache-manifest.6d70d6a46a6173c1acce062306839592.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff49fb6023c346fe9e20dc26f6c46e3",
+    "url": "/Keeper-App-React.git/index.html"
+  },
+  {
+    "revision": "c2e12b47d6e6354fcfb0",
+    "url": "/Keeper-App-React.git/static/js/2.f56f688f.chunk.js"
+  },
+  {
+    "revision": "c01df8dd52db93832723",
+    "url": "/Keeper-App-React.git/static/js/main.e76567e5.chunk.js"
+  },
+  {
+    "revision": "c6475cddbee0aa8be25e",
+    "url": "/Keeper-App-React.git/static/js/runtime-main.9f9d7311.js"
+  }
+]);
