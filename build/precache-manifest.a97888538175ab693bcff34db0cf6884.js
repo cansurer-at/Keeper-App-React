14bec429f392355a2eeca2f3192268ca13f7998d
Can Surer <cansurer@outlook.com>
2022-03-11 19:12:51 +0300

    fix bug

---
+++ build/precache-manifest.a97888538175ab693bcff34db0cf6884.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d607629cccd4fed49ce2d2ca457e34a4",
+    "url": "/Keeper-App-React/index.html"
+  },
+  {
+    "revision": "c2e12b47d6e6354fcfb0",
+    "url": "/Keeper-App-React/static/js/2.f56f688f.chunk.js"
+  },
+  {
+    "revision": "c01df8dd52db93832723",
+    "url": "/Keeper-App-React/static/js/main.e76567e5.chunk.js"
+  },
+  {
+    "revision": "2ab54a4fe717dfaf27f3",
+    "url": "/Keeper-App-React/static/js/runtime-main.f2ec0275.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Keeper-App-React.git/precache-manifest.6d70d6a46a6173c1acce062306839592.js"
+  "/Keeper-App-React/precache-manifest.a97888538175ab693bcff34db0cf6884.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keeper-App-React.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keeper-App-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
